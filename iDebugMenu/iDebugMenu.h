864f8809f086fb4eacd8881a234b96c888453c20
toxa95401@gmail.com <>
2021-08-08 22:40:18 +0300

    Add base framework.

---
+++ iDebugMenu/iDebugMenu.h
@@ -0,0 +1,18 @@
+//
+//  iDebugMenu.h
+//  iDebugMenu
+//
+//  Created by Anton Paliakou on 8/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iDebugMenu.
+FOUNDATION_EXPORT double iDebugMenuVersionNumber;
+
+//! Project version string for iDebugMenu.
+FOUNDATION_EXPORT const unsigned char iDebugMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iDebugMenu/PublicHeader.h>
+
+
